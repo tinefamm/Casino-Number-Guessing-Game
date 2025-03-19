3dbe8a34768e20375e7c6b0f796abe0e20c9cf0a
tinefam <tinelli.francesco06@gmail.com>
2025-03-20 00:10:54 +0100

    Add comments and avoid every wrong input

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <random>
 #include <math.h>
+#include <ctype.h>
 
-std::random_device rd;
+// To have a better randomity, I've tried this generation
+std::random_device rd; 
 std::mt19937 mt(rd());
 
 class Casino
@@ -12,7 +14,7 @@ private:
 	float bet;
 	int random_number;
 public:
-	Casino(int difficulty, float bet)
+	Casino(int difficulty, float bet) // Every time an Object is createad, it generate his random number
 		: difficulty(difficulty), bet(bet)
 	{
 		std::uniform_int_distribution<int> uniform_dist(1, 10 * difficulty);
@@ -21,7 +23,7 @@ public:
 
 	float reward()
 	{
-		return this->bet * std::pow(10.0f, difficulty);
+		return this->bet * std::pow(10.0f, difficulty); 
 	}
 
 	int maxNumber() const 
@@ -41,27 +43,51 @@ public:
 int main()
 {
 	int difficulty, num;
-	float bet;
+	float bet = 0;
 		
 	do
 	{
 		std::cout << "Please enter a difficulty bewtween 1 to 3:" << std::endl;
 		std::cin >> difficulty;
+
+		if (std::cin.fail()) // Check if it's an integer
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Svuota il buffer
+			std::cout << "Invalid input. Please enter a number between 1 and 3." << std::endl;
+			difficulty = 0;
+		}
 	} 
-	while (difficulty < 1 || difficulty > 3);
+	while (difficulty != 1 && difficulty != 2 && difficulty != 3);
 	
 	do
 	{
 		std::cout << "How much do you want to bet?" << std::endl;
 		std::cin >> bet;
+
+		if (std::cin.fail()) // Check if it's an integer
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Svuota il buffer
+			std::cout << "Invalid input. Please enter a positive number." << std::endl;
+			bet = 0;
+		}
 	} while (bet <= 0);
 	
-	Casino player(difficulty, bet);
+	Casino player(difficulty, bet); // Create the "profile"
 
 	do
 	{
 		std::cout << "Choose a number between 1 and " << player.maxNumber() << ": " << std::endl;
 		std::cin >> num;
+
+		if (std::cin.fail()) // Check if it's an integer
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Svuota il buffer
+			std::cout << "Invalid input. Please enter a positive number between 1 and " << player.maxNumber() << std::endl;
+			bet = 0;
+		}
 	} while (num < 1 || num > player.maxNumber());
 
 	player.winCondition(num);
